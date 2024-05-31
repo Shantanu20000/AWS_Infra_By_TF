3224fcb884dcb6b144632b26cbb3f8f5ff66089f
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-05-31 13:47:30 +0530

    tf file

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  key_name      = "shan_pvt_key"
+}
