5f5a1ad82505770c01a2f0e0ccee10f8fb3b0251
user.email : "shantanuvanarase36@gmail.com <shantanuvanarase36@gmail.com>
2024-06-01 18:49:41 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami                    = "ami-00beae93a2d981137"
   instance_type          = var.instance_type
-  key_name               = "tf-key"
+  key_name               = "aws_key_pair."
   vpc_security_group_ids = [aws_security_group.allow_http.id]
 }
