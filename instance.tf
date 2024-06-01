d3c64acaa2a6fee928b325eb2e845c6b4a7c4f25
user.email : "shantanuvanarase36@gmail.com <shantanuvanarase36@gmail.com>
2024-06-01 19:04:55 +0530

    --- Auto Git Commit ---

--- instance.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-resource "aws_instance" "web" {
-  ami           = "ami-04b70fa74e45c3917"
-  instance_type = "t2.micro"
-  key_name      = "shan_pvt_key"
-}
