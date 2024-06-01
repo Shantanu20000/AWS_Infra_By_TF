b012d97d2f0d082cb7a5cf7021feae34989c90bd
user.email : "shantanuvanarase36@gmail.com <shantanuvanarase36@gmail.com>
2024-06-01 18:31:27 +0530

    --- Auto Git Commit ---

--- inst_sg.tf
+++ inst_sg.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "allow_http" {
       from_port = port.value
       to_port = port.value
       protocol = "tcp"
-      cidr_blocks = []
+      cidr_blocks = ["0.0.0.0/0"]
     }
 }
   egress {
