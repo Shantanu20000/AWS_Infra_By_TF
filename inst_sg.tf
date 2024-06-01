e656d3934fbd8259b4019597d4923d8fdb2dfcc4
user.email : "shantanuvanarase36@gmail.com <shantanuvanarase36@gmail.com>
2024-06-01 18:26:36 +0530

    --- Auto Git Commit ---

--- inst_sg.tf
+++ inst_sg.tf
@@ -3,7 +3,10 @@ resource "aws_security_group" "allow_http" {
   description = "Allow HTTP inbound traffic"
   vpc_id      = "vpc-03e1d2655a1d23f73" # Replace with your VPC ID
 
-
+dynamic "ingress" {
+  
+  
+}
 
   egress {
     from_port   = 0
