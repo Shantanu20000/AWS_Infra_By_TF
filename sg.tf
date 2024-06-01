38b5d797a4ce97b62a595de62a6848df39415a76
user.email : "shantanuvanarase36@gmail.com <shantanuvanarase36@gmail.com>
2024-06-01 19:19:50 +0530

    --- Auto Git Commit ---

---
+++ sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_port" {
+  name        = "allow_port"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = "vpc-03e1d2655a1d23f73" # Replace with your VPC ID
+
+  dynamic "ingress" {
+    for_each = [ 22, 80, 443 ,3306 ]
+    iterator = prot
+    content {
+      description = "TLC from VPC"
+      from_port = port.value
+      to_port = port.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+  
