abd49e1348152d73a17a60661cb14e462796104b
user.email : "shantanuvanarase36@gmail.com <shantanuvanarase36@gmail.com>
2024-06-01 17:31:46 +0530

    tf file

--- sg_inst.tf
+++ sg_inst.tf
@@ -26,6 +26,13 @@ resource "aws_security_group" "allow_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
