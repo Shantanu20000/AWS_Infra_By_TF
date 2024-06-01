dcc8036807817b38bafc2b92a47312b54f08251b
user.email : "shantanuvanarase36@gmail.com <shantanuvanarase36@gmail.com>
2024-06-01 18:45:30 +0530

    --- Auto Git Commit ---

--- inst_sg.tf
+++ inst_sg.tf
@@ -14,7 +14,11 @@ resource "aws_security_group" "allow_http" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
-  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
   
-}
