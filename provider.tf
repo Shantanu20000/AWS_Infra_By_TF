74fde99fe2949a0df3b264d17814fe1ff49476c9
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-06-02 14:17:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIAR5N5RCH7FOABEYXE"
-  secret_key = ""
+  region = "${var.region}"
+  access_key = "${var.user_access_key}"
+  secret_key = "${var.user_secret_key}"
 }
